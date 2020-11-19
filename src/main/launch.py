64ed9e08ce72a65bc22e01727c3f9ba016f01200
Hugo Bois <hugo.bois@hotmail.fr>
2020-11-19 16:35:03 +0100

    added: command and error handling

--- src/main/launch.py
+++ src/main/launch.py
@@ -1,6 +1,16 @@
 import discord
 from discord.ext import commands
 
+prefix = "?"
+
+def help():
+    print("help")
+
+def cmd2():
+    print("clmd2")
+
+cmds = {'': cmd2,'help': help}
+
 def get_token(file):
     file = open(file, "r")
     return file.read()
@@ -10,7 +20,13 @@ class Client(discord.Client):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+        cmd = message.content.split(' ')
+        if not cmd[0].startswith("mom" + prefix):
+            return
+        try:
+            cmds[cmd[0][4:]]()
+        except:
+            print(f"La commande {cmd[0]} n'existe pas")
 
 client = Client()
 client.run(get_token("token"))
