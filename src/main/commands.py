d24d58646c95cf54493515c36d1a65e85a71f823
erwan.vivien <erwan.vivien@epita.fr>
2020-11-19 22:00:07 +0100

    awaited the error_messages (previous was bugged without it

--- src/main/commands.py
+++ src/main/commands.py
@@ -3,7 +3,7 @@ import time
 import datetime
 
 
-async def error_message():
+async def error_message(message):
     embed = discord.Embed(title="Wrong arguments",
                           colour=discord.Colour(0x42aff2),
                           description="Please check ``help`` for more information",
@@ -21,31 +21,31 @@ def author_name(author):
 
 async def default(self, message, args):
     if args:
-        return await error_message():
+        return await error_message(message)
     return 0
 
 
 async def set(self, message, args):
     if not args or len(args) != 1:
-        return await error_message():
+        return await error_message(message)
     return 0
 
 
 async def next(self, message, args):
     if args:
-        return await error_message():
+        return await error_message(message)
     return 0
 
 
 async def week(self, message, args):
     if not args or len(args) >= 2 or not args[0].isdigit():
-        return error_message()
+        return await error_message(message)
     return 0
 
 
 async def prefix(self, message, args):
     if not args or len(args) != 1:
-        return error_message()
+        return await error_message(message)
     return 0
 
 
