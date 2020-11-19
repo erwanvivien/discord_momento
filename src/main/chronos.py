52f197aa0547a3a88175f1d2c38804b604a8565b
erwan.vivien <erwan.vivien@epita.fr>
2020-11-19 22:00:35 +0100

    moved content of calendar.py to chronos.py

--- src/main/chronos.py
+++ src/main/chronos.py
@@ -9,6 +9,24 @@ import requests
 import ics
 
 
+def get_year():
+    y = datetime.datetime.now().year
+    return y + 2 if datetime.datetime.now().month < 7 else y + 3
+
+
+STUDENT_PROM = get_year()
+ASSISTANT_PROM = STUDENT_PROM - 2
+OUTPUT = '.'
+CALDIR = os.path.join(OUTPUT, 'calendars')
+
+
+def get_calendar(promo, group):
+    output = '{}/{}.ics'.format(CALDIR, group)
+    cal = chronos.chronos(promo, group)
+    with open('{}'.format(output), 'w') as out:
+        out.writelines(cal)
+
+
 API = 'https://v2ssl.webservices.chronos.epita.net/api/v2'
 AUTH_TOKEN = 'r1KEMfOZsVfjSEzWTTUoD5Zy'
 RANGE = 5
